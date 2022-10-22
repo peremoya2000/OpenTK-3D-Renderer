6baaeaf1f0234537619f928a7297cc570e10aace
peremoya2000 <48447693+peremoya2000@users.noreply.github.com>
2022-10-22 19:18:30 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenTK_3D_Renderer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
