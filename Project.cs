a868100bf648af4b775a21f5cfc98247d1000543
PereM-CB <48447693+peremoya2000@users.noreply.github.com>
2026-04-06 14:11:47 +0200

    Small fix

--- Project.cs
+++ Project.cs
@@ -1,13 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.IO;
 
 namespace OpenTK_3D_Renderer
 {
     static class Project
     {
-        public const string Root = "../../../";
-        public const string Resources = Root + "Resources/";
+        public static readonly string Root = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "../../../"));
+        public static readonly string Resources = Path.Combine(Root, "Resources/");
         public const string DefaultTex = "defaultTex.png";
     }
 }
